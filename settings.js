a3efe4e62b9cb560378e585f7cea508a5a09a7fc
Toki_mui7798 <crisvegar2@gmail.com>
2025-11-17 12:00:21 -0500

    Crear settings.js

---
+++ settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: "Made in ñam",
+    prefix: [".", "/", "#", "!"],
+    owner: ["573001234567@s.whatsapp.net"],
+    version: "1.0.0",
+    autoread: true,
+    logs: true,
+    botNumber: null
+};
