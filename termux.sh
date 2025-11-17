4dbceda160fb2e828a070d43f858fdd0a8164142
Toki_mui7798 <crisvegar2@gmail.com>
2025-11-17 09:39:12 -0500

    Actualizar termux.sh

--- termux.sh
+++ termux.sh
@@ -1,17 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Actualizar paquetes
 pkg update && pkg upgrade
 
-# Instalar Node.js y npm
 pkg install nodejs
 
-# Clonar el repositorio (ajusta la URL a tu repositorio)
 git clone https://tu-repositorio.git
 cd tu-repositorio
 
-# Instalar dependencias
-npm install
-
-# Iniciar el bot
 node index.js
